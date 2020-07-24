5f3d35faeed8d9fe48544df6cd81e5b7083bc56e
davidqatraining <david.j.mccartney@gmail.com>
2020-07-24 13:38:46 +0000

    Fixed variables changes index.html

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -7,5 +7,5 @@ import random
 def home():
     response = requests.get('http://service_4:5003/piratename')
     print(response)
-    sentence = response.text
+    piratename = response.text
     return render_template('index.html', piratename = piratename, title = 'Home')
