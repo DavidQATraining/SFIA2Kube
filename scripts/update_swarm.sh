5c0295b82f2798192f6379294abceb905e1fdace
davidqatraining <david.j.mccartney@gmail.com>
2020-07-28 11:48:51 +0000

    adding push and pull to update

--- scripts/update_swarm.sh
+++ scripts/update_swarm.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-docker push
+docker push davidqatraining/service_1
+docker push davidqatraining/service_2
+docker push davidqatraining/service_3
+docker push davidqatraining/service_4
+docker pull davidqatraining/service_1
+docker pull davidqatraining/service_2
+docker pull davidqatraining/service_3
+docker pull davidqatraining/service_4
 docker stack deploy --compose-file docker-compose.yml projectstack
