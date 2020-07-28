9e005cc54b48284d94febcb6f1750cdfcdba7cf8
davidqatraining <david.j.mccartney@gmail.com>
2020-07-28 11:56:45 +0000

    adding update commands to updateswarm

--- scripts/update_swarm.sh
+++ scripts/update_swarm.sh
@@ -11,6 +11,6 @@ docker pull davidqatraining/service_3
 docker pull davidqatraining/service_4
 #docker stack deploy --compose-file docker-compose.yml projectstack
 docker service update --image service_1:latest service_1
-docker service update --image service_1:latest service_2
-docker service update --image service_1:latest service_3
-docker service update --image service_1:latest service_4
+docker service update --image service_2:latest service_2
+docker service update --image service_3:latest service_3
+docker service update --image service_4:latest service_4
