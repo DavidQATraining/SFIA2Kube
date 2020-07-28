f48c528f696d69ac63b3e67c2eeee7a96ac7111d
davidqatraining <david.j.mccartney@gmail.com>
2020-07-28 12:08:54 +0000

    added push and pull to build images took it out of update

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,17 +2,29 @@
 
 # if service 1 image does not exsist locally build image, then same for 2,3,4
 if [[ "$(docker images - q davidqatraining/service_1:latest 2> /dev/null)" == "" ]]; then
-    docker build -t davidqatraining/service_1 ./Service_1 
+    docker build -t davidqatraining/service_1 ./Service_1
+    docker push davidqatraining/service_1
+else
+    docker pull davidqatraining/service_1
 fi
 
 if [[ "$(docker images - q davidqatraining/service_2:latest 2> /dev/null)" == "" ]]; then
-    docker build -t davidqatraining/service_2 ./Service_2 
+    docker build -t davidqatraining/service_2 ./Service_2
+    docker push davidqatraining/service_2
+else
+    docker pull davidqatraining/service_2
 fi
 
 if [[ "$(docker images - q davidqatraining/service_3:latest 2> /dev/null)" == "" ]]; then
-    docker build -t davidqatraining/service_3 ./Service_3 
+    docker build -t davidqatraining/service_3 ./Service_3
+    docker push davidqatraining/service_3
+else
+    docker pull davidqatraining/service_3
 fi
 
 if [[ "$(docker images - q davidqatraining/service_4:latest 2> /dev/null)" == "" ]]; then
-    docker build -t davidqatraining/service_4 ./Service_4 
+    docker build -t davidqatraining/service_4 ./Service_4
+    docker push davidqatraining/service_4
+else
+    docker pull davidqatraining/service_4
 fi
--- scripts/update_swarm.sh
+++ scripts/update_swarm.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-#docker-compose push
-docker push davidqatraining/service_1
-docker push davidqatraining/service_2
-docker push davidqatraining/service_3
-docker push davidqatraining/service_4
-docker pull davidqatraining/service_1
-docker pull davidqatraining/service_2
-docker pull davidqatraining/service_3
-docker pull davidqatraining/service_4
-#docker stack deploy --compose-file docker-compose.yml projectstack
+
 docker service update --image service_1:latest service_1
 docker service update --image service_2:latest service_2
 docker service update --image service_3:latest service_3
