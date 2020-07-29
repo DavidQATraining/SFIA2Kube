d57abb336bfe482720443739a370c9f99c196b1b
davidqatraining <david.j.mccartney@gmail.com>
2020-07-29 09:56:28 +0000

    cleaning up test text

--- scripts/build_swarm.sh
+++ scripts/build_swarm.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# if stack does not exsist locally build stack
-#if [[ "$(docker stack services projectstack 2> /dev/null)" == "" ]]; then
-#    docker stack deploy --compose-file docker-compose.yml projectstack
- 
-#fi
 
 docker stack deploy --compose-file docker-compose.yml projectstack
  
