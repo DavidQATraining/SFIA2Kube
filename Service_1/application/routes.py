103257c5f420c7c88644f0472d8f0788dc064ebd
davidqatraining <david.j.mccartney@gmail.com>
2020-07-31 09:43:06 +0000

    fixing database stuff

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -17,22 +17,25 @@ import os
 # export MYSQL_HOST=34.105.241.68
 # export MYSQL_PORT=3306
 # export MYSQL_DB_NAME=sfia2db
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB_NAME')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
-#                                         environ.get('MYSQL_USER') + \
-#                                         ':' + \
-#                                         environ.get('MYSQL_PASSWORD') + \
-#                                         '@' + \
-#                                         environ.get('MYSQL_HOST') + \
-#                                         ':' + \
-#                                         environ.get('MYSQL_PORT') + \
-#                                         '/' + \
-#                                         environ.get('MYSQL_DB_NAME')
+
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+# app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+# app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+# app.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT')
+# app.config['MYSQL_DB'] = os.getenv('MYSQL_DB_NAME')
+
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
+                                        environ.get('MYSQL_USER') + \
+                                        ':' + \
+                                        environ.get('MYSQL_PASSWORD') + \
+                                        '@' + \
+                                        environ.get('MYSQL_HOST') + \
+                                        ':' + \
+                                        environ.get('MYSQL_PORT') + \
+                                        '/' + \
+                                        environ.get('MYSQL_DB_NAME')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
