2d98f060f564381ad228f6d556c5e329d48752eb
davidqatraining <david.j.mccartney@gmail.com>
2020-08-04 09:33:56 +0000

    added config for ssh and trying to show latest pirate name with order by

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -58,7 +58,7 @@ def home():
         pirate_name=piratename
     )
     print(piratename)
-    print_data = pirate_names.query.all()
+    print_data = pirate_names.query.all().order_by('id').reverse()
     db.session.add(post_data)
     db.session.commit()
     return render_template('index.html', piratename = piratename, print_data = print_data, title = 'Home')
