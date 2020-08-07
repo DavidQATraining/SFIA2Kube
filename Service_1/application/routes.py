5123cf5754df4d36b86705a0f902fb5a37de8a5d
davidqatraining <david.j.mccartney@gmail.com>
2020-08-07 08:39:57 +0000

    fixed db to allow 50 chars

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -60,7 +60,7 @@ def home():
 
 class pirate_names(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    pirate_name = db.Column(db.String(30), nullable=False)
+    pirate_name = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return ''.join(
