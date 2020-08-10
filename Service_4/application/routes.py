d5486e48337a98683684853b7ffed83b4169e241
davidqatraining <david.j.mccartney@gmail.com>
2020-08-10 10:31:15 +0000

    demo changes

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -7,8 +7,8 @@ def sentence():
     first_name = requests.get('http://service_2:5001/piratename')
     last_name = requests.get('http://service_3:5002/piratename')
     
-    pirateName = first_name.text + " " + last_name.text
+    #pirateName = first_name.text + " " + last_name.text
     
     # change for presentation
-    #pirateName = "Presenting "+ first_name.text + " " + last_name.text
+    pirateName = "Presenting "+ first_name.text + " " + last_name.text
     return pirateName
